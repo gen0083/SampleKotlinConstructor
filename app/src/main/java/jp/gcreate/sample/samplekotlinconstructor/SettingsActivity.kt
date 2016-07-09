23b2f8d18ebf222ab0fff32ac47276efb1dc79b2
Gen <archiherewego@gmail.com>
2016-07-09 14:10:48 +0900

    Add activity for settings

---
+++ app/src/main/java/jp/gcreate/sample/samplekotlinconstructor/SettingsActivity.kt
@@ -0,0 +1,12 @@
+package jp.gcreate.sample.samplekotlinconstructor
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
