0848e07b42f8863a5de86ef31c3fbeeaab54a3c1
Gen <archiherewego@gmail.com>
2016-07-09 14:36:52 +0900

    Add option menu to open settings

--- app/src/main/java/jp/gcreate/sample/samplekotlinconstructor/MainActivity.kt
+++ app/src/main/java/jp/gcreate/sample/samplekotlinconstructor/MainActivity.kt
@@ -1,12 +1,36 @@
 package jp.gcreate.sample.samplekotlinconstructor
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
-o
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // read from shared preferences
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.open_settings -> {
+                val intent = Intent(this, SettingsActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
     }
 }
--- app/src/main/java/jp/gcreate/sample/samplekotlinconstructor/SettingsActivity.kt
+++ app/src/main/java/jp/gcreate/sample/samplekotlinconstructor/SettingsActivity.kt
@@ -8,5 +8,11 @@ class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        if (savedInstanceState == null) {
+            fragmentManager.beginTransaction()
+                    .replace(R.id.container, SettingsFragment())
+                    .commit()
+        }
     }
 }
