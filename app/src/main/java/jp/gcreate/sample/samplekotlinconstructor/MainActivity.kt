93760d3fa5cdf28dcf079741f3807949f6ece7d5
Gen <archiherewego@gmail.com>
2016-07-09 14:44:29 +0900

    Show value in shared preferences

--- app/src/main/java/jp/gcreate/sample/samplekotlinconstructor/MainActivity.kt
+++ app/src/main/java/jp/gcreate/sample/samplekotlinconstructor/MainActivity.kt
@@ -2,20 +2,31 @@ package jp.gcreate.sample.samplekotlinconstructor
 
 import android.content.Intent
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var textView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textView = findViewById(R.id.from_preferences) as TextView
     }
 
     override fun onStart() {
         super.onStart()
         // read from shared preferences
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        val editTextPreference = sharedPreferences.getString("text_preference", "test")
+        val editIntPreference = sharedPreferences.getInt("int_preference", 0)
+        val editIntOverloadsPreference = sharedPreferences.getInt("int_overloads_preference", 0)
+        textView.text = "$editTextPreference\n" +
+                "$editIntPreference\n" +
+                "$editIntOverloadsPreference"
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
